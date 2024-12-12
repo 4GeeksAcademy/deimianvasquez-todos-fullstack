4d6c4dea7e914cae0412f257f74b821d5072e5ea
Deimian Vásquez <deimianvasquez@gmail.com>
2024-12-12 00:25:58 +0000

    register user

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,49 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# ruta donde registramos un usuario
+@api.route('/register', methods=['POST'])
+def add_new_user():
+    try:
+        body = request.json
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        email = body.get("email", None)
+        name = body.get("name", None)
+        password = body.get("password", None)
 
-    return jsonify(response_body), 200
+        if email is None or password is None or name is None:
+            return jsonify("Email, Password and Name required"), 400
+        
+        else:
+            user = User()
+            user_exist = user.query.filter_by(email=email).one_or_none()
+            
+            if user_exist is not None:
+                return jsonify("User exists"), 400
+
+            password = generate_password_hash(password)
+
+            user.name = name
+            user.email = email
+            user.password = password
+            user.salt=1
+            db.session.add(user)
+
+            try:
+                db.session.commit()
+                return jsonify("user created success"), 201
+            except Exception as err:
+                return jsonify(f"Error: {err.args}"), 500
+    except Exception as err:
+        return jsonify(f"Error: {err.args}"), 500
+    
+
+
